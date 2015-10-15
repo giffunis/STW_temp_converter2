a4bfac38cac8998f034b2b59f34b2668948acf9a
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-10-16 00:25:28 +0100

    modificado el tests/tests.js y ya funcionan

--- tests/tests.js
+++ tests/tests.js
@@ -1,39 +1,34 @@
 var expect = chai.expect;
 
-describe(Pruebas BDD, function() {
-  var temp = new Temperatura(0,"F");
-  var temp2 = new Temperatura(3.2e1,"f")
-  describe("constructor", function() {
+describe("Pruebas BDD", function() {
+  var temp = new Temperatura(0,0);
+  var temp2 = new Temperatura(0,0);
+
+
+  describe("Probando los sets", function() {
     it("Debe valer el tipo F", function() {
+      temp.set_tipo("F");
       expect(temp.tipo).to.equal("F");
     });
 
     it("Debe valer el valor 20", function() {
+      temp.set_valor(20);
       expect(temp.valor).to.equal(20);
     });
   });
 
-  describe("sets", function() {
-    it("Debería cambiar el valor a 30", function() {
-      temp.set_valor(30);
-      expect(temp.get_valor()).to.equal(20);
-    });
-
-    it("El tipo debería ser distinto de F", function() {
-      temp.set_tipo("C");
-      expect(temp.get_tipo()).not.to.equal("F");
-    });
-  });
-
   describe("conversiones", function() {
-    it("3,2e1f = 0.0C", function() {
+    it("3.2e1f = 0.0C", function() {
+      temp2.set_valor(3.2e1);
+      temp2.set_tipo("f");
       var salida = temp2.f_to_c();
       expect(salida).to.equal("0.0C");
     });
 
-    it("El tipo debería ser distinto de F", function() {
+    it("0.0C = 32.0F", function() {
       var salida = temp2.c_to_f();
       expect(salida).to.equal("32.0F");
     });
   });
+
 });
