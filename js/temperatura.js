f733c7c92b04fd4b5e1d1a471d1cb66cb32aaf28
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-10-15 22:13:26 +0100

    definida la clase Medida

---
+++ js/temperatura.js
@@ -0,0 +1,13 @@
+"use stric";
+
+function Medida(valor){
+  // Declaración de variables
+  this.valor = valor;
+  this.tipo = tipo;
+
+  // Funciones de la clase Medida
+  this.get_valor() = function(){return this.valor;}
+  this.get_tipo() = function(){return this.tipo;}
+  this.set_valor(valor) = function(){this.valor = valor;}
+  this.set_tipo(tipo) = function(){this.tipo = tipo;}
+}
