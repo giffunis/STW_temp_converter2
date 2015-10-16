273d4f58451f8eaaebb0136d3922683ff3fc2017
alu0100770551 <alu0100770551@ull.edu.es>
2015-10-16 10:15:39 +0000

    arreglado los test

--- tests/tests.js
+++ tests/tests.js
@@ -48,10 +48,15 @@ describe("Pruebas BDD", function() {
   });
 
   describe("convertir()", function() {
-    it("get_valor", function() {
+    it("convertir(32F)", function() {
       entrada.value = "32F";
       convertir();
       expect(salida.innerHTML).to.equal("0.0C");
+    });
+    it("convertir(0C)", function() {
+      entrada.value = "0C";
+      convertir();
+      expect(salida.innerHTML).to.equal("32.0F");
     });
   }); 
 
